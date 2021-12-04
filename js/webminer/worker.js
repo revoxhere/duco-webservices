a341ada3f8a4d3dcca551fdcad175283736ccbdf
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-12-05 01:22:38 +0200

    Add files via upload

--- js/webminer/worker.js
+++ js/webminer/worker.js
@@ -1,7 +1,6 @@
-importScripts("hashes.js");  
+importScripts("hashes.js");
 
-function getTime()
-{
+function getTime() {
     let date = new Date();
     let h = date.getHours();
     let m = date.getMinutes();
@@ -14,90 +13,78 @@ function getTime()
     return h + ":" + m + ":" + s;
 }
 
-onmessage = function (event)
-{
-    if (event.data.startsWith("Start"))
-    {
+onmessage = function(event) {
+    if (event.data.startsWith("Start")) {
         let getData = event.data.split(",");
-
+        let result = 0;
         let username = getData[1];
         let rigid = getData[2];
         let workerVer = getData[3];
-    
-        if (rigid === "")
-        {
+        let wallet_id = getData[4];
+
+        if (rigid === "") {
             rigid = "None";
         }
 
-        function connect()
-        {
-            var socket = new WebSocket("wss://server.duinocoin.com:14808");
+        function connect() {
+            var socket = new WebSocket("wss://magi.duinocoin.com:14808");
 
-            socket.onmessage = function(event)
-            {
+            socket.onmessage = function(event) {
                 var serverMessage = event.data;
-                if (serverMessage.includes("2."))
-                {
+                if (serverMessage.includes("2.")) {
                     console.log(`${getTime()} | ` + "CPU" + workerVer + ": Connected to node. Server is on version " + serverMessage);
                     socket.send("JOB," + username + ",LOW");
-                }
-                else if (serverMessage.includes("GOOD"))
-                {
+                } else if (serverMessage.includes("GOOD")) {
                     console.log(`${getTime()} | ` + "CPU" + workerVer + ": Share accepted:" + result);
                     postMessage("GoodShare");
                     socket.send("JOB," + username + ",LOW");
-                }
-                else if (serverMessage.includes("BAD"))
-                {
+                } else if (serverMessage.includes("BAD")) {
                     console.log(`${getTime()} | ` + "CPU" + workerVer + ": Share rejected: " + result);
                     postMessage("BadShare");
                     socket.send("JOB," + username + ",LOW");
-                }
-                else if (serverMessage.includes("This user doesn't exist"))
-                {
+                } else if (serverMessage.includes("This user doesn't exist")) {
                     console.log(`${getTime()} | ` + "CPU" + workerVer + ": User not found!");
                     postMessage("Error");
-                }
-                else if (serverMessage.length > 40)
-                {
+                } else if (serverMessage.includes("Too many workers")) {
+                    console.log(`${getTime()} | ` + "CPU" + workerVer + ": Too many workers");
+                    postMessage("Error");
+                } else if (serverMessage.length > 40) {
                     console.log(`${getTime()} | ` + "CPU" + workerVer + ": Job received: " + serverMessage);
                     job = serverMessage.split(",");
                     difficulty = job[2];
                     postMessage("UpdateDiff," + difficulty + "," + workerVer);
 
                     startingTime = performance.now();
-                    for (result = 0; result < 100 * difficulty + 1; result++)
-                    {
-                        var ducos1 = new Hashes.SHA1().hex(job[0] + result);
-                        if (job[1] === ducos1)
-                        {
+                    for (result = 0; result < 100 * difficulty + 1; result++) {
+                        let ducos1 = new Hashes.SHA1().hex(job[0] + result);
+                        if (job[1] === ducos1) {
                             endingTime = performance.now();
                             timeDifference = (endingTime - startingTime) / 1000;
                             hashrate = (result / timeDifference).toFixed(2);
 
-                            postMessage("UpdateLog," + `${getTime()} | ` + "CPU" + workerVer + ": Share found: " + result + " Time: " + timeDifference + " Hashrate: " + hashrate + "<br>");
-                            console.log(`${getTime()} | ` + "CPU" + workerVer + ": Share found: " + result + " Time: " + timeDifference + " Hashrate: " + hashrate + "H/s");
+                            postMessage("UpdateLog," + `${getTime()} | ` + "CPU" + workerVer + ": Nonce found: " + result + " Time: " + Math.round(timeDifference) + "s Hashrate: " + Math.round(hashrate / 1000) + " kH/s<br>");
+                            console.log(`${getTime()} | ` + "CPU" + workerVer + ": Nonce found: " + result + " Time: " + timeDifference + " Hashrate: " + hashrate + "H/s");
                             postMessage("UpdateHashrate," + timeDifference + "," + hashrate + "," + workerVer);
 
-                            socket.send(result + "," + hashrate + ",Official Webminer v2.6.5," + rigid);
+                            socket.send(result + "," + hashrate + ",Official Web Miner 2.8," + rigid + ",," + wallet_id);
                         }
                     }
+                } else {
+                    console.log(`${getTime()} | ` + "CPU" + workerVer + ": " + serverMessage);
+                    postMessage("Error");
                 }
             }
 
-            socket.onerror = function(event)
-            {
+            socket.onerror = function(event) {
                 console.error("CPU" + workerVer + "WebSocket error observed, trying to reconnect: ", event);
                 socket.close("Reason: Error occured in WebWorker.");
             }
 
-            socket.onclose = function(event)
-            {
+            socket.onclose = function(event) {
                 console.error("CPU" + workerVer + ": WebSocket close observed, trying to reconnect: ", event);
-                setTimeout(function()
-                {
+                setTimeout(function() {
                     connect();
-                }, 5000);
+                }, 15000);
             }
         }
         connect();
