d180ad9f5fae220f3fa6e7ff495d2db1f360eac0
Bilaboz <44091093+Bilaboz@users.noreply.github.com>
2022-02-09 18:37:22 +0100

    add more backgrounds

--- js/wallet/welcometext.js
+++ js/wallet/welcometext.js
@@ -8,7 +8,8 @@ $("#welcometext").text(welcome_texts[Math.random() * welcome_texts.length | 0]);
 
 const backrounds = [
     "img/yenn-sea-1.jpg",
-
+    "https://game-guide.fr/wp-content/uploads/2020/06/AmongTreesScreenshot5.jpg",
+    "https://i.imgur.com/csou8q3.jpeg"
 ];
 $("#background").css("background-image", `url(${backrounds[Math.random() * backrounds.length | 0]})`);
 
